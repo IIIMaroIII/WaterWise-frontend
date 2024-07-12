d7633660e64fb1ca7cf3601d04a53f020afe008b
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-07-12 12:07:18 +0100

    Added forms + validation
    Created basic forms for sign in/up using react hook form + added  compatibility library for Yup validation

--- src/components/Users/SignInForm/SignInForm.jsx
+++ src/components/Users/SignInForm/SignInForm.jsx
@@ -1,7 +1,64 @@
+import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
+import { signIn } from '../../../redux/users/operations';
 import css from './signInForm.module.css';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+
+const emailRegExp = /^[\w.-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+
+const minPasswordLength = 8;
+const maxPasswordLength = 112;
+
+const loginSchema = Yup.object({
+  email: Yup.string()
+    .required('Email is required!')
+    .matches(emailRegExp, 'Entered email address is not valid')
+    .email('Please enter a valid email address!'),
+  password: Yup.string()
+    .required('Password is required!')
+    .min(minPasswordLength, 'Too short')
+    .max(maxPasswordLength, 'Too long'),
+});
 
 const SignInForm = () => {
-  return <div>SignInForm</div>;
+  const dispatch = useDispatch();
+  const onAddContact = values => {
+    reset();
+    console.log(values);
+    // dispatch(signIn(values));
+
+    // Розкомментити, коли буде правильно підключено апі
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+  const onSubmit = data => {
+    console.log(data);
+    onAddContact(data);
+  };
+
+  return (
+    <div>
+      {' '}
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email</label>
+        <input {...register('email')} />
+        <p>{errors.email?.message}</p>
+        <label>Password</label>
+        <input {...register('password')} />
+        <p>{errors.password?.message}</p>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default SignInForm;
--- src/components/Users/SignUpForm/SignUpForm.jsx
+++ src/components/Users/SignUpForm/SignUpForm.jsx
@@ -1,7 +1,76 @@
+import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
+import { signUp } from '../../../redux/users/operations';
 import css from './signUpForm.module.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const emailRegExp = /^[\w.-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+
+const minNameLength = 3;
+const maxNameLength = 50;
+const minPasswordLength = 8;
+const maxPasswordLength = 112;
+
+const registrationSchema = Yup.object({
+  name: Yup.string()
+    .required('Name is required')
+    .min(minNameLength, 'Too short!')
+    .max(maxNameLength, 'Too long!'),
+  email: Yup.string()
+    .required('Email is required!')
+    .matches(emailRegExp, 'Entered email address is not valid')
+    .email('Please enter a valid email address!'),
+  password: Yup.string()
+    .required('Password is required!')
+    .min(minPasswordLength, 'Too short')
+    .max(maxPasswordLength, 'Too long'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Please confirm your password'),
+});
 
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  const dispatch = useDispatch();
+  const onRegisterAccount = values => {
+    reset();
+    console.log(values);
+    // dispatch(signUp(values));
+
+    // Розкомментити, коли буде правильний апі
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registrationSchema),
+  });
+  const onSubmit = data => {
+    console.log(data);
+    onRegisterAccount(data);
+  };
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email</label>
+        <input {...register('email')} />
+        <p>{errors.email?.message}</p>
+        <label>Password</label>
+        <input {...register('password')} />
+        <p>{errors.password?.message}</p>
+        <label>Confirm Password</label>
+        <input type="password" {...register('confirmPassword')} />
+        <p>{errors.confirmPassword?.message}</p>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
